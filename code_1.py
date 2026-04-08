56c2308ba3a56b289e6f3e547e5420ae42fac3b8
Tuana Ekşi <tuanaeks@gmail.com>
2026-04-08 13:21:35 +0300

    kod dosyası değiştirildi

--- code_1.py
+++ code_1.py
@@ -1,2 +1,8 @@
+import time
+# First line of our first code
+print("Hello world!")
 #New line is added
-print ("Hey! I'm changing the file!")
+print ("Hey!I'm changing the file!")for i in range(5,-1,-1)
+print("-->" , i)
+time.sleep(1)
+print("Rocket launched!")
